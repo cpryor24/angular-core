8225205739cc0b6ca78f2837ffb3acaf33c6c661
Chris Pryor <chrispryor@live.com>
2019-01-28 16:57:57 -0700

    export home component to app

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@workshop/material';
+import { HomeModule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { CustomersModule } from './customers/customers.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,7 +18,10 @@ import { MaterialModule } from '@workshop/material';
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HomeModule,
+    CustomersModule,
+    ProjectsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
