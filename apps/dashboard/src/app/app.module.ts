2c272f4a3f6cd117c192d8a6cf055d2cf8a0d50f
Chris Pryor <chrispryor@live.com>
2019-01-29 12:08:37 -0700

    lazy loading

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -3,25 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
-import { RouterModule } from '@angular/router';
+// import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from '@workshop/material';
-import { HomeModule } from './home/home.module';
-import { HomeComponent } from './home/home.component';
-import { CustomersModule } from './customers/customers.module';
-import { ProjectsModule } from './projects/projects.module';
+// import { HomeModule } from './home/home.module';
+// import { HomeComponent } from './home/home.component';
+// import { CustomersModule } from './customers/customers.module';
+// import { ProjectsModule } from './projects/projects.module';
+import { UiLoginModule } from '@workshop/ui-login';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     BrowserAnimationsModule,
     MaterialModule,
-    HomeModule,
-    CustomersModule,
-    ProjectsModule
+    UiLoginModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
